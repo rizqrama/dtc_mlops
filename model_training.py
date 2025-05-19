c2427198cfe4615b45de29122bddc7ac1c84cb6b
rizqrama <rr.rizqiramadhan@gmail.com>
2025-05-20 05:48:23 +0700

    update code

--- model_training.py
+++ model_training.py
@@ -2,20 +2,19 @@
 import duckdb as db
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
 from sklearn.metrics import root_mean_squared_error
 
 
 # %% Load Data
 def get_taxi_data(year: int, month: int) -> pd.DataFrame:
     """
-    Fetch NYC yellow taxi data for a specific year and month and calculate trip duration
+    Fetch NYC yellow taxi data for a specific year and month
+    and calculate trip duration
 
     Args:
         year: The year of the data (e.g., 2023)
@@ -29,18 +28,24 @@ def get_taxi_data(year: int, month: int) -> pd.DataFrame:
     base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data"
     url = f"{base_url}/yellow_tripdata_{year}-{month_str}.parquet"
 
+    # Create table name based on month
+    table_name = f"taxi_data_{year}_{month_str}"
+
+    # Create table with the data and duration calculation
     return con.execute(f"""
+        CREATE OR REPLACE TABLE {table_name} AS
         SELECT *,
             EXTRACT(
                 EPOCH FROM (tpep_dropoff_datetime - tpep_pickup_datetime)
                 ) / 60 AS duration
         FROM read_parquet('{url}')
-    """).df()
+    """)
 
 
+# %%
 # Connect to DuckDB
 con = db.connect("taxi_data.duckdb")
-
+# %%
 # Get data for January through March 2023
 months_to_fetch = [1, 2, 3]
 taxi_data = {}
@@ -49,23 +54,81 @@ for month in months_to_fetch:
     taxi_data[f'df_{month:02d}'] = get_taxi_data(2023, month)
 
 # %%
-df_jan = taxi_data['df_01']
-df_feb = taxi_data['df_02']
-
-# %%
-df_jan.shape
+df_jan = con.sql("SELECT * FROM taxi_data_2023_01").df()
+df_feb = con.sql("SELECT * FROM taxi_data_2023_02").df()
 
 # %%
 df_jan.duration.describe()
-# %%
-df_jan_clean = con.execute("""
-    FROM df_jan
-    WHERE
-        duration >= 1 AND duration <= 60
-""").df()
 
-df_jan_clean.shape
+
 # %%
-round(len(df_jan_clean) / len(df_jan) * 100, 2)
+def df_prep(dataset):
+    df = dataset[(dataset.duration >= 1) & (dataset.duration <= 60)]
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    return df
+
+
+df_train = df_prep(df_jan)
+df_val = df_prep(df_feb)
+
+len(df_train), len(df_val)
+
+# %%
+round(len(df_train) / len(df_jan) * 100, 2)
+
+# %%
+df_train['PU_DO'] = df_train['PULocationID'] + '_' + df_train['DOLocationID']
+df_val['PU_DO'] = df_val['PULocationID'] + '_' + df_val['DOLocationID']
+
+# %%
+categorical = categorical = ['PULocationID', 'DOLocationID']
+numerical = ['trip_distance']
+
+dv = DictVectorizer()
+
+train_dicts = df_train[categorical + numerical].to_dict(orient='records')
+X_train = dv.fit_transform(train_dicts)
+
+val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+X_val = dv.transform(val_dicts)
+
+# %%
+target = 'duration'
+y_train = df_train[target].values
+y_val = df_val[target].values
+
+# %%
+print(f"Number of features: {X_train.shape[1]}")
+
+# %%
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_train)
+
+root_mean_squared_error(y_train, y_pred)
+
+# %%
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_val)
+
+root_mean_squared_error(y_val, y_pred)
+
+# %%
+lr = Lasso(0.01)
+lr.fit(X_train, y_train)
+
+y_pred = lr.predict(X_val)
+
+root_mean_squared_error(y_val, y_pred)
+
+# %%
+sns.histplot(y_pred, kde=True, stat="density", label='prediction')
+sns.histplot(y_val, kde=True, stat="density", label='actual')
+
+plt.legend()
 
 # %%
